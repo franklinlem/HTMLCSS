d6c6e26b95466a85e0509e44d4a4b5068ba3aabc
franklinlem <franklinlem@yahoo.com>
2024-10-25 12:59:36 -0300

    appclima

--- appclima/script.js
+++ appclima/script.js
@@ -1,5 +1,5 @@
 const APIKEY = "46fcaef5ece0306b5c280412b766e25e";
-const APICountryURL = "https://flagsapi.com/";
+const APICountryURL = "https://flagsapi.com";
 
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
@@ -9,15 +9,25 @@ const tempElement = document.querySelector("#temperature span");
 const descElement = document.querySelector("#description");
 const weatherIconElement = document.querySelector("#weather-icon");
 const countryElement = document.querySelector("#country");
-const umidityElement = document.querySelector("#umidity span");
+const humidityElement = document.querySelector("#humidity span");
 const windElement = document.querySelector("#wind span");
+
+const weatherContainer = document.querySelector("#weather-data");
 
 const getWeatherData = async (city) => {
     const resultado = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKEY}&lang=pt_br`;
     const res = await fetch(resultado);
     const data = await res.json();
     console.log(data);
-    cityElement.innerText = city;
+    cityElement.innerText = data.name;
+    tempElement.innerText = Number(data.main.temp).toFixed(1);
+    descElement.innerText = data.weather[0].description;
+    weatherIconElement.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+    countryElement.src = `${APICountryURL}/${data.sys.country}/flat/64.png`;
+    humidityElement.innerText = data.main.humidity+"%";
+    windElement.innerText = data.wind.speed+" m/s";
+
+    weatherContainer.style.display = "block";
 };
 
 
@@ -25,4 +35,12 @@ searchBtn.addEventListener("click", (e) => {
     e.preventDefault();
     const city = cityInput.value;
     getWeatherData(city);
+});
+
+cityInput.addEventListener("keydown", (e) => {
+    if(e.key === "Enter") {
+        e.preventDefault();
+        const city = cityInput.value;
+        getWeatherData(city);
+    }
 });
